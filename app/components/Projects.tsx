a569daa6890b03f09ee84cc2d56e7a1706d544be
Anubhav Ghosh <iamanubhav11@gmail.com>
2025-02-20 23:42:43 +0530

    Updated Code

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -16,7 +16,8 @@ function Projects() {
       github_url: "https://vidtube-flame.vercel.app/",
       live_link: "https://vidtube-flame.vercel.app/",
       youtube_link: "https://vidtube-flame.vercel.app/",
-      image: "https://res.cloudinary.com/dzg7eemfm/image/upload/v1740071714/rpehfykjg1rvlblreg8h.png",
+      image:
+        "https://res.cloudinary.com/dzg7eemfm/image/upload/v1740071714/rpehfykjg1rvlblreg8h.png",
       id: 1,
     },
     {
@@ -27,27 +28,32 @@ function Projects() {
       github_url: "https://diners-find.vercel.app/",
       live_link: "https://diners-find.vercel.app/",
       youtube_link: "https://diners-find.vercel.app/",
-      image: "https://res.cloudinary.com/dzg7eemfm/image/upload/v1740071714/qtq8dsdw3dp9m8onykge.png",
+      image:
+        "https://res.cloudinary.com/dzg7eemfm/image/upload/v1740071714/qtq8dsdw3dp9m8onykge.png",
       id: 2,
     },
     {
-        project_title: "Study Notion",
-        date: "October 2024",
-        description: "",
-        tech_stack: ["Reactjs", "TailwindCSS", "Nodejs", "Express", "MongoDB"],
-        github_url: "https://study-notion-nu-six.vercel.app/",
-        live_link: "https://study-notion-nu-six.vercel.app/",
-        youtube_link: "https://study-notion-nu-six.vercel.app/",
-        image: "https://res.cloudinary.com/dzg7eemfm/image/upload/v1740071714/vhzbojfri4sptnwuh8wa.png",
-        id: 3,
-      },
+      project_title: "Study Notion",
+      date: "October 2024",
+      description: "",
+      tech_stack: ["Reactjs", "TailwindCSS", "Nodejs", "Express", "MongoDB"],
+      github_url: "https://study-notion-nu-six.vercel.app/",
+      live_link: "https://study-notion-nu-six.vercel.app/",
+      youtube_link: "https://study-notion-nu-six.vercel.app/",
+      image:
+        "https://res.cloudinary.com/dzg7eemfm/image/upload/v1740071714/vhzbojfri4sptnwuh8wa.png",
+      id: 3,
+    },
   ];
   return (
     <div className="w-full flex flex-col gap-5 p-5 md:p-10">
       {projectData &&
         projectData.map((data) => {
           return (
-            <div key={data.id} className="flex md:flex-row flex-col w-full md:h-48 gap-4 md:gap-0 rounded-lg p-5 border border-b-4">
+            <div
+              key={data.id}
+              className="flex md:flex-row flex-col w-full md:h-48 gap-4 md:gap-0 rounded-lg p-5 border border-b-4"
+            >
               <div className="flex flex-col gap-2">
                 <div className="flex gap-2 w-fit">
                   <p className="font-semibold">{data.project_title}</p>
@@ -56,9 +62,12 @@ function Projects() {
                   </div>
                 </div>
                 <div className="flex flex-wrap gap-2">
-                  {data.tech_stack.map((tech,index) => {
+                  {data.tech_stack.map((tech, index) => {
                     return (
-                      <div key={index} className="bg-gray-100 text-xs w-fit text-black rounded px-1 py-1">
+                      <div
+                        key={index}
+                        className="bg-gray-100 text-xs w-fit text-black rounded px-1 py-1"
+                      >
                         {tech}
                       </div>
                     );
@@ -82,7 +91,15 @@ function Projects() {
                   </Button>
                 </div>
               </div>
-              <div><Image className="h-32 rounded-lg" src={data.image} alt={data.project_title} /></div>
+              <div>
+                <Image
+                  className="h-32 rounded-lg"
+                  width={350}
+                  height={400}
+                  src={data.image}
+                  alt={data.project_title}
+                />
+              </div>
             </div>
           );
         })}
